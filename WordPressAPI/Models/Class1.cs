7dbc13c520dab68a5a4f80ccb4d7670cf0e67702
Tom Miller <tom@shepardroad.com>
2017-11-20 19:31:42 -0600

    Added Core WebAPI project, did some minor cleanup and rearranging of projects...

--- WordPressAPI/Models/Class1.cs
+++ WordPressAPI/Models/Class1.cs
@@ -1,21 +1,53 @@
-﻿
+﻿using System;
+
 public class Rootobject
 {
-    public Class1[] Property1 { get; set; }
+    public int id { get; set; }
+    public DateTime date { get; set; }
+    public DateTime date_gmt { get; set; }
+    public Guid guid { get; set; }
+    public DateTime modified { get; set; }
+    public DateTime modified_gmt { get; set; }
+    public string slug { get; set; }
+    public string status { get; set; }
+    public string type { get; set; }
+    public string link { get; set; }
+    public Title title { get; set; }
+    public Content content { get; set; }
+    public Excerpt excerpt { get; set; }
+    public int author { get; set; }
+    public int featured_media { get; set; }
+    public string comment_status { get; set; }
+    public string ping_status { get; set; }
+    public bool sticky { get; set; }
+    public string template { get; set; }
+    public string format { get; set; }
+    public object[] meta { get; set; }
+    public int[] categories { get; set; }
+    public int[] tags { get; set; }
+    public _Links _links { get; set; }
 }
 
-public class Class1
+public class Guid
 {
-    public int id { get; set; }
-    public int count { get; set; }
-    public string description { get; set; }
-    public string link { get; set; }
-    public string name { get; set; }
-    public string slug { get; set; }
-    public string taxonomy { get; set; }
-    public int parent { get; set; }
-    public object[] meta { get; set; }
-    public _Links _links { get; set; }
+    public string rendered { get; set; }
+}
+
+public class Title
+{
+    public string rendered { get; set; }
+}
+
+public class Content
+{
+    public string rendered { get; set; }
+    public bool _protected { get; set; }
+}
+
+public class Excerpt
+{
+    public string rendered { get; set; }
+    public bool _protected { get; set; }
 }
 
 public class _Links
@@ -23,7 +55,11 @@ public class _Links
     public Self[] self { get; set; }
     public Collection[] collection { get; set; }
     public About[] about { get; set; }
-    public WpPost_Type[] wppost_type { get; set; }
+    public Author[] author { get; set; }
+    public Reply[] replies { get; set; }
+    public VersionHistory[] versionhistory { get; set; }
+    public WpAttachment[] wpattachment { get; set; }
+    public WpTerm[] wpterm { get; set; }
     public Cury[] curies { get; set; }
 }
 
@@ -42,8 +78,32 @@ public class About
     public string href { get; set; }
 }
 
-public class WpPost_Type
+public class Author
 {
+    public bool embeddable { get; set; }
+    public string href { get; set; }
+}
+
+public class Reply
+{
+    public bool embeddable { get; set; }
+    public string href { get; set; }
+}
+
+public class VersionHistory
+{
+    public string href { get; set; }
+}
+
+public class WpAttachment
+{
+    public string href { get; set; }
+}
+
+public class WpTerm
+{
+    public string taxonomy { get; set; }
+    public bool embeddable { get; set; }
     public string href { get; set; }
 }
 
